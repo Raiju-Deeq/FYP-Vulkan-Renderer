a19c2f92ea0eaa7f2d0ec0259508fd88bf30820f
Raiju-Deeq <mdeeq0@me.com>
2026-03-28 12:26:57 +0000

    Added GaussianSpat class for use later in accordance with my repo structure

---
+++ src/GaussianSplat.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mdeeq0 on 3/28/26.
+//
+
+#include "GaussianSplat.h"
---
+++ src/GaussianSplat.h
@@ -0,0 +1,14 @@
+//
+// Created by mdeeq0 on 3/28/26.
+//
+
+#ifndef VULKAN_RENDERER_GAUSSIANSPLAT_H
+#define VULKAN_RENDERER_GAUSSIANSPLAT_H
+
+
+class GaussianSplat
+{
+};
+
+
+#endif //VULKAN_RENDERER_GAUSSIANSPLAT_H
